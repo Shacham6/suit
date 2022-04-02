188137619496cdaa418fc59a64c86ef3786d953f
Shacham Ginat <shacham.ginat@pagaya.com>
2022-04-02 19:37:51 +0300

    Lets do an updating thing

--- packages/suit/_.suit.py
+++ packages/suit/_.suit.py
@@ -2,8 +2,8 @@ from suit_plugins import black, pytest, install_requires
 
 from suit import suit
 
-lint = suit("lint::black", black.lint("suit"))
-format_ = suit("format::black", black.format_("suit"))
+lint = suit("lint:black", black.lint("suit"))
+format_ = suit("format:black", black.format_("suit"))
 
 tests = suit("tests", pytest.pytest("tests", color="yes"))
 install_requires = suit(
