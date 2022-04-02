36d2397e392303fbbb409fed9383a516a0839044
Shacham Ginat <shacham.ginat@pagaya.com>
2022-04-02 12:58:43 +0300

    Configurations and stuff

--- packages/suit/suit/main.py
+++ packages/suit/suit/main.py
@@ -1,10 +1,11 @@
 import contextlib
 import pathlib
 import re
-from typing import Tuple
+from typing import Iterable, List, Optional, Tuple
 
 import click
 import logbook
+from box import Box
 from rich.text import Text
 
 from suit.collector import collect
@@ -13,9 +14,45 @@ from .runtime import Runtime
 from .scope import Scope
 
 
+def __get_configuration(
+    ctx: click.Context,
+    param: click.Parameter,
+    value: Optional[pathlib.Path],
+) -> Box:
+    # pylint: disable=unused-argument
+    if value:
+        return Box.from_toml(filename=value)
+
+    if found_target := __search_root_file("suit.toml"):
+        return Box.from_toml(filename=found_target)
+
+    return Box()
+
+
+def __search_root_file(filename) -> Optional[pathlib.Path]:
+    for search_location in __walk_backwards(pathlib.Path.cwd()):
+        target = search_location.joinpath(filename)
+        if target.exists():
+            return target
+    return
+
+
+def __walk_backwards(location: pathlib.Path) -> Iterable[pathlib.Path]:
+    """ Walk from location up to the root path of the system. """
+    if location.is_file():
+        location = location.parent
+    yield location
+    yield from location.parents
+
+
 @click.command("suit")
 @click.argument("rules", nargs=-1, type=str)
-def cli(rules: Tuple[str, ...]):
+@click.option("--config",
+              "-c",
+              "config",
+              type=click.Path(exists=True, readable=True, path_type=pathlib.Path),
+              callback=__get_configuration)
+def cli(rules: Tuple[str, ...], config: Box):
     targets = list(collect())
     failures = []
     for target in __filter_target_rules(rules, targets):
@@ -28,9 +65,9 @@ def cli(rules: Tuple[str, ...]):
         with _push_extras(scope=group_log_text):
             try:
                 target.value.invoke(runtime, target_scope)
-            except Exception as e:
-                runtime.exception(e)
-                failures.append((target, e))
+            except Exception as exc:
+                runtime.exception(exc)
+                failures.append((target, exc))
 
 
 @contextlib.contextmanager
