232aec4d562ffabcba0be5e46d4434694779815d
Shacham Ginat <shacham.ginat@pagaya.com>
2022-04-22 10:49:26 +0300

    Collector works

--- packages/suit/src/suit/collector.py
+++ packages/suit/src/suit/collector.py
@@ -1,19 +1,39 @@
 import pathlib
 from typing import Any, Mapping, NamedTuple
 
+import tomli
+
 
 class TargetsCollector:
+    """
+    Collector of Suit Targets.
+    """
+
     def __init__(self, root: pathlib.Path):
         self.__root = root
 
     def collect(self):
-        return [
-            Target()
-        ]
+        """Collect all targets in the directory structure"""
+        for found_project_file in self.__root.glob("**/pyproject.toml"):
+            with found_project_file.open("rb") as found_project_file_io:
+                project_data = tomli.load(found_project_file_io)
+                if not _pyproject_uses_suit(project_data):
+                    continue
+                yield Target(
+                    root=self.__root,
+                    target=found_project_file,
+                    suit=project_data["tool"]["suit"],
+                )
 
 
-class Target:
-    pass
+class Target(NamedTuple):
+    """
+    A suit-using target.
+    """
+
+    root: pathlib.Path
+    target: pathlib.Path
+    suit: Mapping[str, Any]
 
 
 def _pyproject_uses_suit(pyproject_data: Mapping[str, Any]) -> bool:
--- packages/suit/tests/test_collector.py
+++ packages/suit/tests/test_collector.py
@@ -1,9 +1,10 @@
+import io
 from typing import Any, Mapping
 from unittest.mock import MagicMock
-import pytest
 
+import pytest
 import toml
-from suit.collector import TargetsCollector, _pyproject_uses_suit, Target
+from suit.collector import Target, TargetsCollector, _pyproject_uses_suit
 
 
 def pyproject_toml_file_mock(*, path: str, content: Mapping[str, Any]) -> MagicMock:
@@ -14,21 +15,25 @@ def pyproject_toml_file_mock(*, path: str, content: Mapping[str, Any]) -> MagicM
 
     mock.parent = parent_mock
     mock.__str__.return_value = f"{path.strip('/')}/pyproject.toml"
-    mock.read_text.return_value = toml.dumps(content)
+
+    mock.open.return_value = io.BytesIO(toml.dumps(content).encode("utf-8"))
     return mock
 
 
 def test_find_pyproject_toml_with_suit_configured():
     root = MagicMock()
+
+    suit_project_file = pyproject_toml_file_mock(
+        path="packages/suit", content={"tool": {"suit": {}}}
+    )
+
     root.glob.return_value = (
-        pyproject_toml_file_mock(path="packages/suit", content={"tool": {"suit": {}}}),
+        suit_project_file,
         pyproject_toml_file_mock(path="packages/notsuit", content={}),
     )
     collector = TargetsCollector(root)
     results = list(collector.collect())
-    assert results == [
-        Target()
-    ]
+    assert results == [Target(root=root, target=suit_project_file, suit={})]
 
 
 def test_is_pyproject_uses_suit():
