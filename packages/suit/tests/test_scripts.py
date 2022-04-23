382e5180213706c2a853d9f59d474f4c9b72d0bd
Shacham Ginat <shacham.ginat@pagaya.com>
2022-04-23 15:05:32 +0300

    Works

--- packages/suit/tests/test_scripts.py
+++ packages/suit/tests/test_scripts.py
@@ -4,7 +4,8 @@ from typing import Mapping
 import pytest
 from suit.collector import SuitConfig
 from suit.scripts import ShellScript, resolve_scripts
-from suit.targets import ShellScriptSpec, TargetConfig, TargetConfigData
+from suit.scripts.types import CompositeScript
+from suit.targets import CompositeScriptSpec, RefScriptSpec, ShellScriptSpec, TargetConfig, TargetConfigData
 
 
 def test_resolve_scripts():
@@ -12,17 +13,34 @@ def test_resolve_scripts():
         pathlib.Path("root/target"),
         TargetConfigData(
             scripts={
-                "format": ShellScriptSpec("black"),
+                "black": ShellScriptSpec("black"),
+                "lint": CompositeScriptSpec(
+                    [
+                        RefScriptSpec("black"),
+                        ShellScriptSpec("pylint"),
+                    ]
+                ),
             }
         ),
     )
     suit_config = SuitConfig(pathlib.Path("root/"), {}, [target_config])
     scripts = resolve_scripts(suit_config, target_config)
     assert scripts == {
-        "format": ShellScript(
-            name="format",
+        "black": ShellScript(
+            name="black",
             specs=ShellScriptSpec("black"),
             suit=suit_config,
             target=target_config,
-        )
+        ),
+        "lint": CompositeScript(
+            name="lint",
+            specs=CompositeScriptSpec(
+                [
+                    RefScriptSpec("black"),
+                    ShellScriptSpec("pylint"),
+                ]
+            ),
+            suit=suit_config,
+            target=target_config,
+        ),
     }
