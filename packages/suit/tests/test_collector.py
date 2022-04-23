a274c03c8e2eac44b34993591ba5b06b504bf421
Shacham Ginat <shacham.ginat@pagaya.com>
2022-04-23 10:46:59 +0300

    Move things

--- packages/suit/tests/test_collector.py
+++ packages/suit/tests/test_collector.py
@@ -1,6 +1,5 @@
 import io
 import pathlib
-import string
 from typing import Any, Iterable, List, Mapping, cast
 from unittest.mock import MagicMock
 
@@ -12,7 +11,6 @@ from suit.collector import (
     Suit,
     SuitCollector,
     Target,
-    Targets,
     TargetScript,
     _find_root_configuration,
     _pyproject_uses_suit,
@@ -38,9 +36,7 @@ def pyproject_toml_file_mock(*, path: str, content: Mapping[str, Any]) -> MagicM
 def test_find_pyproject_toml_with_suit_configured():
     root = MagicMock()
 
-    suit_project_file = pyproject_toml_file_mock(
-        path="packages/suit", content={"tool": {"suit": {}}}
-    )
+    suit_project_file = pyproject_toml_file_mock(path="packages/suit", content={"tool": {"suit": {}}})
 
     root.glob.return_value = (
         suit_project_file,
@@ -49,9 +45,7 @@ def test_find_pyproject_toml_with_suit_configured():
     collector = SuitCollector(root, {})
     results = collector.collect()
     assert results.root == root
-    assert results.raw_targets == [
-        _TargetConfig(path=suit_project_file.parent, data={})
-    ]
+    assert results.raw_targets == [_TargetConfig(path=suit_project_file.parent, data={})]
 
 
 def test_is_pyproject_uses_suit():
@@ -131,119 +125,3 @@ def test_filter_targets():
     assert __targets_to_names(suit.targets.find("packages/package-a")) == [
         "packages/package-a",
     ]
-
-
-def test_target_script_compiled():
-    suit = Suit(
-        root=pathlib.Path("root/"),
-        project_config={},
-        raw_targets=[
-            _TargetConfig(
-                pathlib.Path("root/packages/package-a"),
-                {
-                    "target": {
-                        "scripts": {"format": "black --config {root.path} {local.path}"}
-                    }
-                },
-            )
-        ],
-    )
-    package_a_target = suit.targets["packages/package-a"]
-    assert package_a_target.scripts == {
-        "format": TargetScript(
-            "black --config {root.path} {local.path}",
-            root=Box(path=pathlib.Path("root/")),
-            local=Box(path=pathlib.Path("root/packages/package-a")),
-            args=Box(),
-        )
-    }
-
-
-def test_compile_target_script():
-    target_config = _TargetConfig(
-        pathlib.Path("root/packages/package-a"),
-        {"target": {"scripts": {"format": "black --config {root.path} {local.path}"}}},
-    )
-    suit = Suit(
-        root=pathlib.Path("root/"), project_config={}, raw_targets=[target_config]
-    )
-
-    result = TargetScript.compile_inline(
-        "black --config {root.path} {local.path}", suit, target_config
-    )
-
-    assert result == TargetScript(
-        cmd="black --config {root.path} {local.path}",
-        root=Box(path=pathlib.Path("root/")),
-        local=Box(path=pathlib.Path("root/packages/package-a")),
-        args=Box(),
-    )
-
-
-def test_execute_script():
-    target_script = TargetScript(
-        "echo 'lol {root.path}'", Box(path=pathlib.Path("root/")), Box(), Box()
-    )
-    script_execution = target_script.execute()
-    assert cast(bytes, script_execution.stdout.read()).decode("utf-8") == "lol root\n"
-
-
-def test_script_inheritance():
-    target_config = _TargetConfig(
-        pathlib.Path("root/packages/package-a"),
-        {
-            "target": {
-                "inherit": ["helloer"],
-            }
-        },
-    )
-    project_config = {
-        "templates": {
-            "helloer": {
-                "scripts": {
-                    "hello": "echo I'm a helloer from {local.path}",
-                }
-            }
-        }
-    }
-    suit = Suit(pathlib.Path("root/"), project_config, [target_config])
-
-    assert suit.targets["packages/package-a"].scripts == {
-        "hello": TargetScript(
-            "echo I'm a helloer from {local.path}",
-            Box(path=pathlib.Path("root/")),
-            Box(path=pathlib.Path("root/packages/package-a")),
-            Box(),
-        )
-    }
-
-
-def test_args_are_passed_in_target():
-    target_config = _TargetConfig(
-        pathlib.Path("root/packages/package-a"),
-        {
-            "target": {
-                "inherit": ["helloer"],
-                "args": {"what_im_here_to_say": "praise the sun!!"},
-            }
-        },
-    )
-    project_config = {
-        "templates": {
-            "helloer": {
-                "scripts": {
-                    "hello": "echo I'm a helloer from {local.path}, and I'm here to say '{args.what_im_here_to_say}'"
-                }
-            }
-        }
-    }
-    suit = Suit(pathlib.Path("root/"), project_config, [target_config])
-
-    assert suit.targets["packages/package-a"].scripts == {
-        "hello": TargetScript(
-            "echo I'm a helloer from {local.path}, and I'm here to say '{args.what_im_here_to_say}'",
-            Box(path=pathlib.Path("root/")),
-            Box(path=pathlib.Path("root/packages/package-a")),
-            Box(what_im_here_to_say="praise the sun!!"),
-        )
-    }
